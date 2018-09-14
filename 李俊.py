2e0705bf88f57bf8f661f2d61c22eb17c6b3e404
18370746992 <43258345+18370746992@users.noreply.github.com>
2018-09-14 11:04:00 +0800

    Add files via upload

---
+++ 李俊.py
@@ -0,0 +1,26 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+from turtle import *
+setup(1000, 500, 200, 200)
+penup()
+fd(-200)
+pendown()
+pencolor("black")
+pensize(30)
+seth(50)
+circle(30, 180)
+pencolor("grey")
+circle(50, 180)
+pencolor("red")
+circle(70, 180)
+pencolor("purple")
+circle(90, 180)
+pencolor("darkgreen")
+circle(110, 180)
+pencolor("gold")
+circle(130, 180)
+
